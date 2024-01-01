23e2ab37dedb0b1d99262ea37bffc035afd7b29b
Roy <lyt2980999208@gmail.com>
2024-01-01 23:12:02 +0800

    feat: input size

--- components/InputBar.vue
+++ components/InputBar.vue
@@ -18,23 +18,21 @@ watchDebounced(input, () => {
     <div
       translate-x="-1/2"
       left="1/2"
-      flex="~ items-center" bg="base" outline="~ 2 gray5/80" fixed bottom-0 z-2 mb-2 of-visible rounded-full px-2 py-1 shadow="~ inner black"
+      flex="~ items-center" bg="base" outline="~ 2 gray5/80" fixed bottom-0 z-2 mb-2 of-visible rounded-full pl-3 shadow="~ inner black"
     >
       <div i-ph-magnifying-glass-duotone text="gray3/60" mr-2 />
       <input
         ref="inputRef"
         v-model="input"
         placeholder="中，英文名，描述"
-        appearance-none
-        bg-transparent
-        text-white
-        outline-none
+        appearance-none bg-transparent py-2 text-white outline-none
         @keyup.enter="inputRef!.blur()"
       >
       <button
         id="settingBtn"
-        text=" gray3 op80"
+        text=" gray3 op80 lg"
         hover="text-op-100"
+        px-3
         @click="settingModalVisible = !settingModalVisible"
       >
         <div i-ph-gear-duotone />
