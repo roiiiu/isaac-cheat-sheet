9d5d0c6d35a5aed0746d4a3c6d87f842c984f414
Roy <lyt2980999208@gmail.com>
2024-01-01 23:01:45 +0800

    feat: detail modal border

--- components/DetailsModal.vue
+++ components/DetailsModal.vue
@@ -7,6 +7,7 @@ const modelValue = defineModel<boolean>({ required: true })
     <div v-if="modelValue" class="fixed inset-0 z-99 flex items-center justify-center px4">
       <div class="absolute inset-0 bg-black opacity-60" @click="modelValue = false" />
       <div
+        border="2 gray5/40"
         class="relative z-99 rounded bg-base p-4 text-wheat"
         min-w="36"
       >
