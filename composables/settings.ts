7d59952d4e5718ac80ebf9a60b4f7c5611557952
Roy <lyt2980999208@gmail.com>
2024-01-10 16:59:02 +0800

    feat: disable ssr

--- composables/settings.ts
+++ composables/settings.ts
@@ -29,7 +29,9 @@ export const useSettingStore = defineStore('settings', () => {
     showBackToTop,
   }
 }, {
-  persist: true,
+  persist: {
+    storage: persistedState.localStorage,
+  },
 })
 
 // if (import.meta.hot)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { pwa } from './config/pwa'
 import { appDescription } from './constants/index'
 
 export default defineNuxtConfig({
+  ssr: false,
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
